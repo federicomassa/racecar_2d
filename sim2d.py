c638c1bb95be6c0c614d9919c5e7ea62aaedff8e
Federico Massa <fedemassa91@gmail.com>
2019-11-18 17:48:30 +0100

    Studying how to write a follow-trajectory model

--- sim2d.py
+++ sim2d.py
@@ -6,6 +6,7 @@ import numpy
 class Player:
     def __init__(self, player_index, image_path, length, model_fcn):
         self.image = Sim2D.get_image(image_path)
+        self.id = -1
         self.length = length
         self.width = self.image.get_height()/self.image.get_width()*length
         self.model_fcn = model_fcn
@@ -19,13 +20,30 @@ class Player:
     def update_pose(self, controls, dT, *argv):
         self.current_pose = self.model_fcn(self.current_pose, controls, dT, *argv)
     
+class Trajectory:
+    def __init__(self):
+        self.points = []
+
+def unicycle_model(current_pose, controls, time_step, *argv):
+    if len(current_pose) != 3:
+        raise Exception("current_pose must be of length 3: x,y,theta")
+    if len(controls) != 2:
+        raise Exception("controls must be of length 2: v, omega")
+
+    new_pose = current_pose
+    new_pose[0] += controls[0]*numpy.cos(current_pose[2])*time_step
+    new_pose[1] += controls[0]*numpy.sin(current_pose[2])*time_step
+    new_pose[2] += controls[1]*time_step
+
+    return new_pose
 
 class Sim2D:
     def __init__(self):
         self.done = False
         self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption('TazioSim 2D')
         self.clock = pygame.time.Clock()
-
+        self.current_time = 0.0
         self.track_json_path = None
         self.scale = 0.05 # m/pixel
         self.zoom_action = 0.9 # Relative increase in scale on scroll
@@ -34,12 +52,16 @@ class Sim2D:
         self.origin = (0.0,0.0)
         self.background_color = (255, 255, 255)
         self.track_color = (0,0,0)
+        self.font_color = (0,0,0)
         self.track_pix_size = 5
 
         # Dictionary of vehicles in the form {player_index, (vehicle_img, vehicle_length)}
         self.players = []
         self.__is_updated = []
         pygame.init()
+
+        self.__font = pygame.font.Font('freesansbold.ttf', 32)
+
 
     def update_player(self, player_index, controls, *argv):
         self.players[player_index].update_pose(controls, 1.0/self.frequency, *argv)
@@ -48,6 +70,7 @@ class Sim2D:
     def add_player(self, image_path, vehicle_length, model_fcn, init_pose):
         self.players.append(Player(len(self.players), image_path, vehicle_length, model_fcn))
         self.players[len(self.players)-1].init_pose(init_pose)
+        self.players[len(self.players)-1].id = len(self.players)
         self.__is_updated.append(False)
 
         # Return player index
@@ -104,7 +127,6 @@ class Sim2D:
 
     # Draw vehicle in position world_pos
     def render_vehicles(self):
-
         for player in self.players:
             image = pygame.transform.scale(player.image, (int(float(player.length)/self.scale), int(float(player.length)/self.scale*player.image.get_height()/player.image.get_width())))
             image = pygame.transform.rotate(image, player.current_pose[2]*180.0/numpy.pi)
@@ -114,8 +136,16 @@ class Sim2D:
             pix_pos = (pix_pos[0] - int(image.get_width()/2.0), pix_pos[1] - int(image.get_height()/2.0))
             self.screen.blit(image, pix_pos)
 
-            # DEBUG
-            pygame.draw.circle(self.screen, (255,0,0), self.world2pix(player.current_pose), 5)
+    def render_ui(self):
+        player_text = 'Player ' + str(self.focus_player.id)
+        time_text ='Time: ' + "{:4.2f}".format(self.current_time)
+
+        player_text_surface = self.__font.render(player_text, True, self.font_color, self.background_color)
+        time_text_surface = self.__font.render(time_text, True, self.font_color, self.background_color)
+
+        player_text_rect = player_text_surface.get_rect()
+        textRect.center = (int(textRect.width/2.0 + 0.05*self.screen.get_width()),int(textRect.height/2.0 + 0.05*self.screen.get_height()))
+        self.screen.blit(text_surface, textRect)
 
     @staticmethod
     def get_image(path):        
@@ -127,6 +157,7 @@ class Sim2D:
         self.screen.fill(self.background_color)
         self.render_track()
         self.render_vehicles()
+        self.render_ui()
 
     def tick(self):
         # Check if all vehicles were updated
@@ -156,15 +187,19 @@ class Sim2D:
 
         # By default, camera follows first player
         if self.focus_player == None and len(self.players) != 0:
-            self.origin = self.players[0].current_pose
+            self.__focus_player(self.players[0])
         elif self.focus_player != None:
             self.origin = self.focus_player.current_pose
 
         self.render()
         
         pygame.display.flip()  
-        self.__sleep()      
+        self.current_time += self.__sleep()/1000.0
+
+        # Reset
+        for i in self.__is_updated:
+            i = False
 
     def __sleep(self):
-        self.clock.tick(self.frequency)
+        return self.clock.tick(self.frequency)
 
