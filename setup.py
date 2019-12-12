e3ea572e023a23d70a882003a82947b045f81587
Federico Massa <fedemassa91@gmail.com>
2019-12-12 10:33:07 +0100

    Testing setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='racecar_2d',
+   version='0.1',
+   description='A 2D racing car simulator',
+   author='Federico Massa',
+   author_email='fedemassa91@gmail.com',
+   packages=['racecar_2d'],
+   install_requires=[],
+)
