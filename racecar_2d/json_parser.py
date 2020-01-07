1c3dfbd010be796370e9ae834bdb5489ab483854
Federico Massa <fedemassa91@gmail.com>
2020-01-07 15:16:10 +0100

    Added check of duplicates in json parser. Test

--- racecar_2d/json_parser.py
+++ racecar_2d/json_parser.py
@@ -1,5 +1,6 @@
 import sys
 from math import sqrt
+from math import abs
 import json
 
 # Parse json file with Inside track, Outside track, Racing track
@@ -11,6 +12,8 @@ def json_parser(json_file, take_every=1):
     ins = []
     out = []
 
+    tol = 0.001
+
     with open(json_file) as data_file:
         data = json.loads(data_file.read())
         o = data['Outside']
@@ -21,20 +24,43 @@ def json_parser(json_file, take_every=1):
                 d = t[i]
                 n = d[0]
                 m = d[1]
-                ins.append((n,m))
+
+                # Check duplicates
+                if abs(n - ins[-1][0]) > tol or abs(m - ins[-1][1]) > tol :
+                    ins.append((n,m))
+
+        # Check duplicates between first and last point
+        if abs(ins[0][0] - ins[-1][0]) < tol and abs(ins[0][1] - ins[-1][1]) < tol :
+            ins = ins[0:-1]            
 
         for j, _ in enumerate(o):
             if j%take_every == 0:
                 c = o[j]
                 x = c[0]
                 y = c[1]
-                out.append((x,y))
+
+                # Check duplicates
+                if abs(x - out[-1][0]) > tol or abs(y - out[-1][1]) > tol :
+                    out.append((n,m))
+
+        # Check duplicates between first and last point
+        if abs(out[0][0] - out[-1][0]) < tol and abs(out[0][1] - out[-1][1]) < tol :
+            out = out[0:-1]       
 
         for e, _ in enumerate(p):
             if e%take_every == 0:
                 z = p[e]
                 g = z[0]
                 h = z[1]
+
+                # Check duplicates
+                if abs(g - race[-1][0]) > tol or abs(h - race[-1][1]) > tol :
+                    race.append((n,m))
+
                 race.append((g,h))
+
+        # Check duplicates between first and last point
+        if abs(race[0][0] - race[-1][0]) < tol and abs(race[0][1] - race[-1][1]) < tol :
+            race = race[0:-1]       
 
     return race,ins,out
